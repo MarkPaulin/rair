2648e6a75f78c675fcf2c375fee8473a5cf30e28
Mark Paulin <paulin.mark@googlemail.com>
2025-05-05 18:16:46 +0100

    vendor dependencies

--- R/format.R
+++ R/format.R
@@ -11,11 +11,6 @@
 #' bad_code <- "x+ 1  *(3 -0 )"
 #' format_code(bad_code)
 #'
-#' tmp <- tempfile(fileext = ".R")
-#' writeLines(tmp, bad_code)
-#' format_file(tmp)
-#' readLines(tmp
-#'
 #' @export
 #' @rdname format
 format_code <- function(code) {
